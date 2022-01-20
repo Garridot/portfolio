fc19e2c380cf2417a544fdc6689baab995693024
Garridot <garridot210@gmail.com>
2022-01-20 19:08:16 -0300

    remove form email

--- portofolio/portofolio/settings.py
+++ portofolio/portofolio/settings.py
@@ -121,11 +121,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-# smtp Configuration
-
-EMAIL_BACKEND       = config('EMAIL_BACKEND')
-EMAIL_HOST          = config('EMAIL_HOST')
-EMAIL_PORT          = config('EMAIL_PORT')
-EMAIL_USE_TLS       = config('EMAIL_USE_TLS')
-EMAIL_HOST_USER     = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
