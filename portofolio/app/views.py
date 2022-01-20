c84c832514bce693e95d2b843f7607a4237baaee
Garridot <garridot210@gmail.com>
2022-01-20 19:11:37 -0300

    remove form email

--- portofolio/app/views.py
+++ portofolio/app/views.py
@@ -3,25 +3,12 @@ from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 from django.core.mail import send_mail
 from django.contrib import messages
-from portofolio.settings import EMAIL_HOST_USER
+
 # Create your views here.
 
 class MainView(TemplateView):
     template_name = "main.html"
 
 
-# def EmailSend(request):
-#     email   = request.POST['email']    
-#     message = request.POST['message']   
-
-#     send_mail(
-#         f'Portfolio Message:{email}',
-#         message,
-#         email,
-#         [EMAIL_HOST_USER,],        
-#     )
-#     messages.success(request,'Gracias! Tratare de responder lo mas rapido que sea posible. ')
-#     return redirect('https://garrido-portfolio.herokuapp.com/#contact')
-    
 
 
